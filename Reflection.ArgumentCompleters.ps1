9b186e23b8ed49e47f1fb9cbfeb6a5f9c13f9b52
bielawb <bartek.bielawski@live.com>
2013-01-25 11:18:42 +0100

    Added Snippet for new completer function.

--- Reflection.ArgumentCompleters.ps1
+++ Reflection.ArgumentCompleters.ps1
@@ -1,12 +1,27 @@
 ## Completers for Reflection module by Jaykul   ##
 ## Using version 4.1 - http://poshcode.org/3174 ##
 
-function Reflection_GetContstructorArgumentCompletion
+function Reflection_TypeParameterArgumentCompletion
 {
     [ArgumentCompleter(
         Parameter = 'Type',
-        Command = 'Get-Constructor',
+        Command = { Get-CommandWithParameter -Module Reflection -ParameterName Type },
         Description = 'Complete type names, for example: Get-Constructor -Type <TAB>')]
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     [System.Management.Automation.CompletionCompleters]::CompleteType($wordToComplete)
+}
+
+function Reflection_AcceleratorNameArgumentCompletion
+{
+    [ArgumentCompleter(
+        Parameter = 'Accelerator',
+        Command = ('Get-Accelerator', 'Remove-Accelerator'),
+        Description = 'Complete accelerators, for example Get-Accelerator -Accelerator <TAB>'
+    )]
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+    Reflection\Get-Accelerator -Accelerator "$wordToComplete*" |
+        ForEach-Object {
+            $toolTip = "Points to type: $($_.Value)"
+            New-CompletionResult $_.Key $toolTip
+        }
 }
