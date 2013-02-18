7b9a1902de4ccec3cb82edb54bcf25da82f0d0b5
Jason Shirk <jason@truewheels.net>
2013-02-17 22:22:27 -0800

    Add installation script.

---
+++ Install.ps1
@@ -0,0 +1,51 @@
+
+param([string]$InstallDirectory)
+
+$fileList = @(
+    'about_TabExpansion++.help.txt',
+    'Hyper-V.ArgumentCompleters.ps1',
+    'Microsoft.PowerShell.Core.ArgumentCompleters.ps1',
+    'Microsoft.PowerShell.Diagnostics.ArgumentCompleters.ps1',
+    'Microsoft.PowerShell.Management.ArgumentCompleters.ps1',
+    'Microsoft.PowerShell.Utility.ArgumentCompleters.ps1',
+    'NetAdapter.ArgumentCompleters.ps1',
+    'PrintManagement.ArgumentCompleters.ps1',
+    'ScheduledTasks.ArgumentCompleters.ps1',
+    'SmbShare.ArgumentCompleters.ps1',
+    'Storage.ArgumentCompleters.ps1',
+    'TabExpansion++.psd1',
+    'TabExpansion++.psm1',
+    'WindowsExe.ArgumentCompleters.ps1',
+    'WindowsExe.Messages.psd1',
+    'Snippets/ArgumentCompleterFunction.snippets.ps1xml'
+)
+
+
+if ('' -eq $InstallDirectory)
+{
+    $personalModules = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules
+    if (($env:PSModulePath -split ';') -notcontains $personalModules)
+    {
+        Write-Warning "$personalModules is not in `$env:PSModulePath"
+    }
+
+    if (!(Test-Path $personalModules))
+    {
+        Write-Error "$personalModules does not exist"
+    }
+
+    $InstallDirectory = Join-Path -Path $personalModules -ChildPath TabExpansion++
+}
+
+if (!(Test-Path $InstallDirectory))
+{
+    $null = mkdir $InstallDirectory
+    $null = mkdir $InstallDirectory\Snippets
+}
+
+$wc = new-object System.Net.WebClient
+$fileList | ForEach-Object {
+    $wc.DownloadFile("https://raw.github.com/lzybkr/TabExpansionPlusPlus/master/$_",
+                     "$installDirectory\$_")
+    }
+
