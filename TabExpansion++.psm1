2f1cd4a163dcb080191eee72a51b6b2de7c31b27
Shay Levy <scriptolog@gmail.com>
2013-07-11 14:44:12 +0300

    Added File property to ArgumentCompleter objects
    I removed the Enable/Disable-TabExpansion functions, replaced tabs with
    4 spaces per TAB character and added the File property to include the
    File name that contains the completer.  I left the Name parameter I've
    added to the Get-ArgumentCompleter function in place.

--- TabExpansion++.psm1
+++ TabExpansion++.psm1
@@ -475,36 +475,37 @@ function Get-ArgumentCompleter
         {
             if($command -like $Name)
             {
-		    $c = $command
-		    if ($command -and $parameter) { $c += ':' }
-		    $description = $descriptions["${c}${parameter}${native}"]
-		    $completer = [pscustomobject]@{
-			Command = $command
-			Parameter = $parameter
-			Native = $native
-			Description = $description
-			ScriptBlock = $scriptblock
-			File = Split-Path -Leaf -Path $scriptblock.File
-		    }
-		    $completer.PSTypeNames.Add('TabExpansion++.ArgumentCompleter')
-		    Write-Output $completer
+                $c = $command
+                if ($command -and $parameter) { $c += ':' }
+                $description = $descriptions["${c}${parameter}${native}"]
+                $completer = [pscustomobject]@{
+                Command = $command
+                Parameter = $parameter
+                Native = $native
+                Description = $description
+                ScriptBlock = $scriptblock
+                File = Split-Path -Leaf -Path $scriptblock.File
+            }
+            
+            $completer.PSTypeNames.Add('TabExpansion++.ArgumentCompleter')
+            Write-Output $completer
             }
         }
     
         foreach ($pair in $options.CustomArgumentCompleters.GetEnumerator())
         {
-	    if ($pair.Key -match '^(.*):(.*)$')
-	    {
-		$command = $matches[1]
-		$parameter = $matches[2]
-	    }
-	    else
-	    {
-		$parameter = $pair.Key
-		$command = ""
-	    }
+        if ($pair.Key -match '^(.*):(.*)$')
+        {
+        $command = $matches[1]
+        $parameter = $matches[2]
+        }
+        else
+        {
+        $parameter = $pair.Key
+        $command = ""
+        }
 
-	    WriteCompleter $command $parameter $false $pair.Value
+        WriteCompleter $command $parameter $false $pair.Value
         }
 
         foreach ($pair in $options.NativeArgumentCompleters.GetEnumerator())
@@ -1012,19 +1013,6 @@ $descriptions = @{}
 $completionPrivateData = @{}
 
 
-function Enable-TabExpansion
-{
-	$script:options = Get-Variable optionsClone -ValueOnly -Scope Script
-	Remove-Variable optionsClone -Scope Script
-}
-
-function Disable-TabExpansion
-{
-	$script:optionsClone = Get-Variable options -ValueOnly -Scope Script
-	Remove-Variable options -Scope Script
-}
-
-
 # Define the default display properties for the objects returned by Get-ArgumentCompleter
 $typeData = new-object System.Management.Automation.Runspaces.TypeData "TabExpansion++.ArgumentCompleter"
 [string[]]$properties = echo Command Parameter Native Description
@@ -1038,4 +1026,4 @@ $backgroundResultsQueue = new-object System.Collections.Concurrent.ConcurrentQue
 Update-ArgumentCompleter -AsJob
 
 Export-ModuleMember Get-ArgumentCompleter, Register-ArgumentCompleter,
-                    Set-TabExpansionOption, Update-ArgumentCompleter,*-TabExpansion
+                    Set-TabExpansionOption, Update-ArgumentCompleter
