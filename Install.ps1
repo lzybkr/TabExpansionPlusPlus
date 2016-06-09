cf624963038c321b3acbd967803a26efcad287e1
Kurt Roggen [BE] <kurt.roggen@telenet.be>
2016-06-10 00:52:04 +0200

    Update to include 'PowerShellDirect.ArgumentCompleters.ps1'

--- Install.ps1
+++ Install.ps1
@@ -35,6 +35,7 @@ $fileList = @(
     'NetTCPIP.ArgumentCompleters.ps1'
     'Pnpdevice.ArgumentCompleters.ps1'
     'PowerShellWebAccess.ArgumentCompleters.ps1'
+    'PowerShellDirect.ArgumentCompleters.ps1'
     'PrintManagement.ArgumentCompleters.ps1'
     'RobocopyExe.ArgumentCompleters.ps1'
     'ScheduledTasks.ArgumentCompleters.ps1'
